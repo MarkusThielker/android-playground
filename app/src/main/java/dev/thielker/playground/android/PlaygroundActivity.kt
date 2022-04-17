85553ef37707ac0e9b9a3ec248534d31db234657
Markus Thielker <mail.markus.thielker@gmail.com>
2022-04-17 16:45:36 +0200

    Initial commit

---
+++ app/src/main/java/dev/thielker/playground/android/PlaygroundActivity.kt
@@ -0,0 +1,38 @@
+package dev.thielker.playground.android
+
+import android.app.Application
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.core.view.WindowCompat
+import dev.thielker.playground.android.ui.PlaygroundApp
+import dev.thielker.playground.android.ui.theme.PlaygroundTheme
+import timber.log.Timber
+
+class PlaygroundApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
+
+@ExperimentalMaterial3Api
+class PlaygroundActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+
+            PlaygroundTheme {
+
+                PlaygroundApp()
+            }
+        }
+    }
+}
