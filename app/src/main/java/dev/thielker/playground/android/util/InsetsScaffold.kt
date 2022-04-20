eaebb4aa0c08d0f44d7eedd581fb2a83f1c54be2
Markus Thielker <mail.markus.thielker@gmail.com>
2022-04-20 23:18:16 +0200

    feat(layout): limited scaffold content to space between top and bottom bar

--- app/src/main/java/dev/thielker/playground/android/util/InsetsScaffold.kt
+++ app/src/main/java/dev/thielker/playground/android/util/InsetsScaffold.kt
@@ -1,12 +1,17 @@
+@file:Suppress("functionName")
+
 package dev.thielker.playground.android.util
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
@@ -16,8 +21,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 
 @ExperimentalMaterial3Api
@@ -33,19 +44,26 @@ fun InsetsScaffold(
     navigationBarColor: Color = MaterialTheme.colorScheme.surface,
     backgroundColor: Color = MaterialTheme.colorScheme.background,
     contentColor: Color = contentColorFor(backgroundColor),
-    content: @Composable (PaddingValues) -> Unit,
+    content: @Composable BoxScope.() -> Unit,
 ) {
+
+    var topBarHeight by remember { mutableStateOf(0) }
+    var bottomBarHeight by remember { mutableStateOf(0) }
 
     Scaffold(
         modifier = modifier,
         topBar = {
-            Column {
+            Column(
+                modifier = Modifier.onGloballyPositioned { topBarHeight = it.size.height }
+            ) {
                 StatusBarInset(statusBarColor)
                 topBar()
             }
         },
         bottomBar = {
-            Column {
+            Column(
+                modifier = Modifier.onGloballyPositioned { bottomBarHeight = it.size.height }
+            ) {
                 bottomBar()
                 NavigationBarInset(navigationBarColor)
             }
@@ -54,7 +72,17 @@ fun InsetsScaffold(
         floatingActionButton = floatingActionButton,
         floatingActionButtonPosition = floatingActionButtonPosition,
         contentColor = contentColor,
-        content = content
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = with(LocalDensity.current) { topBarHeight.toDp() },
+                        bottom = with(LocalDensity.current) { bottomBarHeight.toDp() },
+                    ),
+                content = content,
+            )
+        }
     )
 }
 
